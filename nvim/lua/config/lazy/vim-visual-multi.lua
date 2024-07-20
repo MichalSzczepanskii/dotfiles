d24b61b08a4935a53636b124079d9c0c603528de
Michał <szczepanskimichal99@gmail.com>
2024-07-20 14:53:33 +0200

    Nvim - multi line plugin

---
+++ nvim/lua/config/lazy/vim-visual-multi.lua
@@ -0,0 +1,11 @@
+return {
+	"mg979/vim-visual-multi",
+	init = function()
+		vim.g.VM_default_mapping = 0
+		vim.g.VM_maps = {
+			["Find Under"] = "<M-j>",
+			["Remove Region"] = "<M-J>",
+		}
+		vim.g.VM_add_cursor_at_pos_no_mappings = 1
+	end,
+}
